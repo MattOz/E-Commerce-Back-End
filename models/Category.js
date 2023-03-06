b56fe8a4e088e4bf9edf0da94626620de3d26792
Matt Osborne <MattJOzzy@gmail.com>
2023-03-05 22:26:46 -0600

    imported starter code and worked on models

---
+++ models/Category.js
@@ -0,0 +1,30 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection.js');
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Category extends Model {}
+// Set up fields and rules for Category model
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
---
+++ models/Product.js
@@ -0,0 +1,46 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+// set up fields and rules for Product model
+Product.init(
+  {
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+    tagIds: {
+      type: DataTypes.ARRAY(DataTypes.DECIMAL),
+      references: {
+        model: 'productTag',
+        key: 'id',
+      },
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
